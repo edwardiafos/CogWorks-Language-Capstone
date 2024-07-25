b4eef9fd1789bed085e5066c0cef3cb9814280ec
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2024-07-25 14:20:10 -0600

    Update match_caption_to_image.py

--- match_caption_to_image.py
+++ match_caption_to_image.py
@@ -7,7 +7,7 @@ def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images
     """
     res = np.dot(semantic_embedding_caption, semantic_embedding_images) # (N,200)
     res_sorted = np.argsort(res, axis=0)
-    print(semantic_embedding_images[:4])
+    top_image_embeddings = semantic_embedding_images[:k]
 
-    return
+    #return
     #get using the image using database given top 4 semantic embeddings
