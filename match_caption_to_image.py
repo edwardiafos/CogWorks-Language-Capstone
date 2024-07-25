dcc50cadb5d0cf8894f175aa112f1cf430dd26a9
BW7425 <bryanw7425@gmail.com>
2024-07-25 16:31:27 -0400

    changed size

--- match_caption_to_image.py
+++ match_caption_to_image.py
@@ -1,11 +1,11 @@
 import numpy as np
 def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images, k=4):
     """
-    caption embedding (200,)
+    caption embedding (1,200)
     all embedded images (N,200) ?
     returns k most similar images
     """
-    res = np.dot(semantic_embedding_caption, semantic_embedding_images) # (N,)
+    res = np.dot(semantic_embedding_caption, semantic_embedding_images) # (N,200)
     res_sorted = np.argsort(res, axis=0)
     top_image_embeddings = semantic_embedding_images[:k]
 
