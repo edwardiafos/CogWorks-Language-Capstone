c74c914fe94a6a495089b53ea6c3c9f9f938b1be
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2024-07-25 13:48:05 -0600

    Update match_caption_to_image.py

--- match_caption_to_image.py
+++ match_caption_to_image.py
@@ -1,8 +1,13 @@
 import numpy as np
-def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images):
-    res = np.dot(semantic_embedding_caption, semantic_embedding_images)
-    res_sorted = np.sort(res, 0)
-    print(res_sorted[:4])
+def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images, k=4):
+    """
+    caption embedding (200,)
+    all embedded images (N,200) ?
+    returns k most similar images
+    """
+    res = np.dot(semantic_embedding_caption, semantic_embedding_images) # (N,200)
+    res_sorted = np.argsort(res, axis=0)
+    print(semantic_embedding_images[:4])
 
     return
     #get using the image using database given top 4 semantic embeddings
