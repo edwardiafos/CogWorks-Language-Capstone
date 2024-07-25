1e2c5061a98907b5ddb768f0bde93ad576d1b033
BW7425 <bryanw7425@gmail.com>
2024-07-25 14:58:59 -0400

    caption to image

---
+++ match_caption_to_image.py
@@ -0,0 +1,8 @@
+import numpy as np
+def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images):
+    res = np.dot(vector1, vector2)
+    res_sorted = np.sort(res, 0)
+    print(res_sorted[:4])
+
+    return()
+    #get using the image using database given top 4 semantic embeddings
