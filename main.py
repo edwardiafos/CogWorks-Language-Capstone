9b86e97bbea835b0b1124b39536fbe4ebaa35183
Manya Tandon <manyatandon11@gmail.com>
2024-07-25 13:03:39 -0400

    framework for helper class, and main function

---
+++ main.py
@@ -0,0 +1,13 @@
+from cogworks_data.language import get_data_path
+
+from gensim.models import KeyedVectors
+filename = "glove.6B.200d.txt.w2v"
+
+# this takes a while to load -- keep this in mind when designing your capstone project
+glove = KeyedVectors.load_word2vec_format(get_data_path(filename), binary=False)
+
+
+# Load saved image descriptor vectors
+import pickle
+with Path(get_data_path('resnet18_features.pkl')).open('rb') as f:
+    resnet18_features = pickle.load(f)
