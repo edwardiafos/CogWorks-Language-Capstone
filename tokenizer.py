2e34369f08320ffe924726128fb8cce487bd6e6c
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2024-07-25 14:09:57 -0600

    remove stop-words from captions (optionally)

--- tokenizer.py
+++ tokenizer.py
@@ -2,10 +2,21 @@ import string
 from pathlib import Path
 import json
 
-def process_caption(caption):
+with open(get_data_path("stopwords.txt"), 'r') as r:
+    stops = []
+    for line in r:
+        stops += [i.strip() for i in line.split('\t')]
+
+def process_caption(caption, remove_stops=False):
+    
+    # optionally remove stop-words, default at False for speed purposes
+    
     caption = caption.translate(str.maketrans('', '', string.punctuation))
     caption = caption.lower()
 
     words = caption.split()
+    
+    if remove_stops:
+        words = [w for w in words if w not in stops]
 
     return words
