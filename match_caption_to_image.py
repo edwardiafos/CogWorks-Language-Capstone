a870d888f4de2dc32e9e2e07ef4aa7d53865f917
edwardiafos <edwardiafos@gmail.com>
2024-07-25 14:36:08 -0500

    updated match_caption_to_image.py

--- match_caption_to_image.py
+++ match_caption_to_image.py
@@ -4,5 +4,5 @@ def match_caption_to_image(semantic_embedding_caption, semantic_embedding_images
     res_sorted = np.sort(res, 0)
     print(res_sorted[:4])
 
-    return()
+    return
     #get using the image using database given top 4 semantic embeddings
