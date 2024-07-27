e335a9b2dfb35751e2960ffe6302ee647f6b8cb6
Ye Yint Hmine <83378804+Ye-Yint-Nyo-Hmine@users.noreply.github.com>
2024-07-27 10:55:56 -0400

    Update resnet_loading.py

--- resnet_loading.py
+++ resnet_loading.py
@@ -3,13 +3,15 @@ from pathlib import Path
 import pickle
 
 
-resnet18_features = {}
+
 
 def load_resnet():
-    global resnet18_features
+    resnet18_features = {}
     with Path(get_data_path('resnet18_features.pkl')).open('rb') as f:
         resnet18_features = pickle.load(f)
+    return resnet18_features
 
-load_resnet()
 
-print(len(resnet18_features))
+if __name__ == '__main__':
+    #* I modified the resnet to load non globally, bc I got errors in functions
+    resnet18_features = load_resnet()
