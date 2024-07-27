6cc83586fe73fa70629cbe6f34a4d77a5a16bbdf
Ye Yint Hmine <83378804+Ye-Yint-Nyo-Hmine@users.noreply.github.com>
2024-07-27 10:54:49 -0400

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,23 +1,26 @@
 import string
 from pathlib import Path
-import json
 from cogworks_data.language import get_data_path
 
-with open(get_data_path("stopwords.txt"), 'r') as r:
-    stops = []
-    for line in r:
-        stops += [i.strip() for i in line.split('\t')]
+# Load stop words from file
+def load_stopwords(file_path):
+    with open(file_path, 'r') as file:
+        return set(line.strip() for line in file)
 
-def process_caption(caption, remove_stops=False):
-    
-    # optionally remove stop-words, default at False for speed purposes
-    
+stopwords_path = get_data_path("stopwords.txt")
+stopwords = load_stopwords(stopwords_path)
+
+def process_caption(caption: str, remove_stops: bool = False) -> list:
+
+    #* Remove punctuation and convert to lowercase
     caption = caption.translate(str.maketrans('', '', string.punctuation))
     caption = caption.lower()
 
+    #* Split the caption into words
     words = caption.split()
-    
+
+    #* Optionally remove stop words
     if remove_stops:
-        words = [w for w in words if w not in stops]
+        words = [word for word in words if word not in stopwords]
 
     return words
